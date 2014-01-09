efdf0978a1bf5eb498774631a02504baeae8150b
Cass Everitt <cass@xyzw.us>
2014-01-09 00:22:42 -0600

    print stuff still not working

--- src/regal/RegalPrint.h
+++ src/regal/RegalPrint.h
@@ -98,12 +98,28 @@ inline void AppendString( const std::string & s, char * buf, int capacity, int &
   return AppendBytes( s.c_str(), int(s.size()), buf, capacity, sz );
 }
 
+template < int N >
+inline void AppendString( char const (&s)[N], char * buf, int capacity, int & sz ) {
+  if( (capacity - sz) <= 1 ) {
+    return;
+  }
+  return AppendBytes( &s[0], N, buf, capacity, sz );
+}
+
 template <>
 inline void AppendString( const char * const & s, char * buf, int capacity, int & sz ) {
   if( (capacity - sz) <= 1 ) {
     return;
   }
   AppendBytes( s, int(strlen(s)), buf, capacity, sz );
+}
+
+template <>
+inline void AppendString( const char & s, char * buf, int capacity, int & sz ) {
+  if( (capacity - sz) <= 1 ) {
+    return;
+  }
+  AppendBytes( &s, 1, buf, capacity, sz );
 }
 
 template <>
@@ -175,20 +191,51 @@ std::string print_array( const T * t, int num, const char *quote = "\"", const c
 
 // doesn't actually trim yet
 template <typename T>
-std::string print_trim( const T * t, char delim, int num, const char *prefix, const char *suffix ) {
+std::string print_trim( const T * t, char delim, unsigned int num, const char *prefix, const char *suffix ) {
   std::string s = prefix;
+  while( num ) {
+    const T * t2 = t;
+    while( *t2 != delim && *t2 != 0 ) {
+      t2++;
+    }
+    if( t2 != 0 ) { // include delim if t2 is delim
+      t2++;
+    }
+    s += std::string( t, t2 - t );
+    if( *t2 == 0 ) {
+      break;
+    }
+    num--;
+    t = t2 + 1;
+  }
   s += t;
   // can skip the suffix if I'm not actually trimming
   // s += suffix;
   return s;
 }
 
+inline std::string print_raw( const void * p, const size_t size ) {
+  char h[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+  const char *cp = reinterpret_cast< const char * >( p );
+  std::string s;
+  s.reserve( size * 2 + size / 4 );
+  for( int i = 0; i < size; i++ ) {
+    s += h[ (cp[i] >> 4) & 0xf ];
+    s += h[ (cp[i] >> 0) & 0xf ];
+    if( (i & 0xf) == 0xf ) {
+      s += ' ';
+    }
+  }
+  if( ( size & 0x3 ) == 0x3 ) {
+    s.erase( s.end()-- );
+  }
+  return s;
+}
 
 
-#define print_optional( ... ) std::string(" implement print_optional() ")
-#define print_raw( ... ) std::string( " implement print_raw() " )
-#define print_right( a, ... ) (a)
-#define print_left( a, ... ) (a)
+#define print_optional( ... ) print_string(" implement print_optional() ")
+#define print_right( a, ... ) print_string(a)
+#define print_left( a, ... ) print_string(a)
 
 
 
